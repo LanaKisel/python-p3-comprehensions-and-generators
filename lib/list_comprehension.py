31e1574b03417f70caabd042b7841d4a88b1d401
Lana Kisel <svetlana.kisel.ca@gmail.com>
2024-01-15 21:53:48 -0800

    list_comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
-
+def return_evens(num_list):  
+    return_evens = [n for n in num_list if (n % 2 == 0)]
+    return (return_evens)
 def make_exclamation(sentence_list):
     pass
+    make_exclamation = [str + "!" for str in sentence_list]
+    return make_exclamation
